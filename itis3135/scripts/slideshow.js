1eb01fa0ed1c9388c84e9f1510d814a0a4c575c4
kwaldro3 <kwaldro7@charlotte.edu>
2025-04-13 14:03:35 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
+    let slideIndex = 1;
+
+    function showSlides(n) {
+        let slides = $(".mySlides");
+        if (n > slides.length) { slideIndex = 1; }
+        if (n < 1) { slideIndex = slides.length; }
+        slides.hide();
+        slides.eq(slideIndex - 1).show();
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
     showSlides(slideIndex);
 });
-
-let slideIndex = 1;
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let slides = $(".mySlides");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-    slides.hide();
-    slides.eq(slideIndex - 1).show();
-}

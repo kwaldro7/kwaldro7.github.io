57efd8edeb54cca8d4066f1afc732bbb839f2a3b
kwaldro3 <kwaldro7@charlotte.edu>
2025-04-14 14:57:40 -0400

    slideshow stuff

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,25 +1,28 @@
+let slideIndex = 1;
+
+function showSlides(n) {
+    let slides = $(".mySlides");
+    if (n > slides.length) slideIndex = 1;
+    if (n < 1) slideIndex = slides.length;
+    slides.hide();
+    slides.eq(slideIndex - 1).show();
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 $(document).ready(function() {
-    let slideIndex = 1;
-
-    function showSlides(n) {
-        let slides = $(".mySlides");
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        slides.hide();
-        slides.eq(slideIndex - 1).show();
-    }
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
     showSlides(slideIndex);
+    $(".prev").click(function() { plusSlides(-1); });
+    $(".next").click(function() { plusSlides(1); });
+    $(".thumbnail-container a").click(function(e) {
+        e.preventDefault();
+        let index = $(this).index() + 1;
+        currentSlide(index);
+    });
 });

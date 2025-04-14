aaa6afc6e2f2f78491d423cdbc92ee12a81ba4a4
kwaldro3 <kwaldro7@charlotte.edu>
2025-04-14 15:00:30 -0400

    Update slideshow.js

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -2,8 +2,12 @@ let slideIndex = 1;
 
 function showSlides(n) {
     let slides = $(".mySlides");
-    if (n > slides.length) slideIndex = 1;
-    if (n < 1) slideIndex = slides.length;
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     slides.hide();
     slides.eq(slideIndex - 1).show();
 }
@@ -18,8 +22,12 @@ function currentSlide(n) {
 
 $(document).ready(function() {
     showSlides(slideIndex);
-    $(".prev").click(function() { plusSlides(-1); });
-    $(".next").click(function() { plusSlides(1); });
+    $(".prev").click(function() {
+        plusSlides(-1);
+    });
+    $(".next").click(function() {
+        plusSlides(1);
+    });
     $(".thumbnail-container a").click(function(e) {
         e.preventDefault();
         let index = $(this).index() + 1;
